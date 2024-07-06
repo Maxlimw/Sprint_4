45c43002108612e72e31913ced7d6a59086d5de8
Maxlimw <maxlimw@yandex.ru>
2024-07-06 20:39:16 +0300

    set default browser

--- src/test/java/tests/BasicTest.java
+++ src/test/java/tests/BasicTest.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 import java.util.concurrent.TimeUnit;
 
 public abstract class BasicTest {
@@ -13,12 +15,24 @@ public abstract class BasicTest {
 
     @Before
     public void setUp() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+    String browser = System.getProperty("browser", "chrome");
+
+        switch (browser.toLowerCase()) {
+        case "firefox":
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+            break;
+        case "chrome":
+        default:
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+            break;
+    }
+
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.get("https://qa-scooter.praktikum-services.ru/");
         driver.findElement(By.className("App_CookieButton__3cvqF")).click();
-    }
+}
 
     @After
     public void teardown() {
